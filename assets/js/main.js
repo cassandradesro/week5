83cfc939031efe192184944f8b8adfbe8102cc46
cassandradesro <cdesro10@gmail.com>
2017-05-07 02:56:57 -0400

    Beginning Homework

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,97 @@
 console.log("Hello World from main.js!");
+/*
+Week 5 - Exercises
+Follow the instruction for each one of these exactly. If it says "a function that..." you need to write a function with the code inside it. 
+If it says "a variable..." then you just make a variable, without a function. 
+This way you can write one, then call the function from the console to test it. 
+Or call the function right after where you wrote it, and then when you're done testing it, comment out the function call.
+Either way, when you move on to the next one, your console wonâ€™t be cluttered up by the last.
+Unless otherwise stated, all these functions need logical names. Remember to name them based on what they do.
+*/
+
+// A function that receives 2 arguments (numbers), adds them together, and console logs the answer.
+
+
+// A function that receives 1 argument (numbers), multiplies it by 2, and returns the answer.
+
+
+// A variable set to an array of strings, each one a common first name.
+
+
+// A function that console logs the first item in an array (the array you created above), then takes that item and moves it 
+// to the back of the array. Calling the function over and over should log each item in the array in turn, going back to the 
+// start after the last item, infinitely.
+
+
+// A variable set to an array of numbers.
+
+
+// A function that finds the highest number in an array of numbers and console logs it.
+
+
+// A function that receives two strings, trims them (look it up), and then returns true if they match AND are not empty, 
+// otherwise returns false. This is a common function used in user registration forms, for the password and password again fields.
+
+
+// A function that rounds any number to the nearest multiple of 42 and returns it. This one has some math in it.
+
+
+// A function that logs â€œThe answer to the question is â€ followed by whatâ€™s returned from the above function with 37 
+// as the argument.
+
+
+// A function that receives one argument, and logs something different depending of if the number is above 212, below 32, 
+// or in between. 
+
+
+// A function that receives 3 strings, a noun, a verb, and an adjective, and logs a sentence constructed with those words 
+// inserted.
+
+
+// A function that receives one argument, an array, and returns true if the array has more than 3 items, false if not.
+
+
+// A function that receives one argument, an array of numbers, and returns true if any item in the array equals 42.
+
+
+// A function that receives two arguments, a number and a string, and logs the string to the console the number of times 
+// in the number argument.
+
+
+// A function that console logs a phrase that gets funnier to more you read it.
+
+
+// A function that sets a timeout for 5 seconds, which calls the above function.
+
+
+// A function that has a loop that happens 5 times, and each time it sets a timeout, each timeout a different random length 
+// between 1 and 5 seconds, to call the funny phrase function. (look up Math.random for random number generation)
+
+
+// A variable set to an array containing every letter in the alphabet (as strings) and every number for 0 through 9.
+
+
+// A function that picks a random item from the above array and returns it.
+
+
+// A function that calls the above function 8 times, each time adding the returned value to a string, generating an eight 
+// character long random alphanumeric string.
+
+
+// A variable called counter, thatâ€™s just the number 0.
+
+
+// A function that adds 0.5 to that last number variable, then console logs it, but only if the number is still below 1000.
+
+
+// A function that sets an interval for 1/30th of a second to call that last function. Once you get that working, 
+// consider how you might use logic like this to animate something on a webpage, such as the css left value of an element, 
+// making it slide to the right. Yep, animation is not really that complex.
+
+
+// A function that cancels that timeout. Obviously, youâ€™ll need to call it pretty quickly after starting the timeout to see the effect.
+
+
+// Bonus: Here's some code to fetch a DIV from the DOM, and adjust it's css left value. 
+// Work that into your interval function to make some animation happen.
+	// document.getElementById("myDiv").style.left = "100px";
